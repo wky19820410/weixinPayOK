401afc06cb8438d0e0c03fdbf5415818a498caa6
wenkaiying <wky19820410@163.com>
2015-08-25 18:50:42 +0800

    sss

--- WeiXinTested/FirstViewController.m
+++ WeiXinTested/FirstViewController.m
@@ -8,6 +8,7 @@
 
 #import "FirstViewController.h"
 #import "WXApi.h"
+#import "AFNetworking.h"
 
 @interface FirstViewController ()
 
@@ -26,6 +27,47 @@
 }
 
 - (IBAction)weiXinPay:(id)sender {
+/*
+    NSString *postUrl = @"INPUT YOUR URL HERE";
+    
+    NSDictionary *parameters = @{@"PARAMETERS NAME 1": @"PARAMETERS VALUE 1",
+                                 @"PARAMETERS NAME 2": @"PARAMETERS VALUE 2"};
+    
+    AFHTTPRequestOperationManager *manager = [AFHTTPRequestOperationManager manager];
+    
+    //方法一：
+    //    manager.requestSerializer = [AFJSONRequestSerializer serializer];
+    //    manager.responseSerializer = [AFJSONResponseSerializer serializer];
+    //    [manager.requestSerializer setValue:@"application/json" forHTTPHeaderField:@"Accept"];
+    //    [manager.requestSerializer setValue:@"application/json; charset=utf-8" forHTTPHeaderField:@"Content-Type"];
+    
+    //注意：默认的Response为json数据
+    //    [manager setResponseSerializer:[AFXMLParserResponseSerializer new]];
+    //    manager.responseSerializer = [AFHTTPResponseSerializer serializer];//使用这个将得到的是NSData
+    manager.responseSerializer = [AFJSONResponseSerializer serializer];//使用这个将得到的是JSON
+    
+    //注意：此行不加也可以
+    manager.responseSerializer.acceptableContentTypes = [NSSet setWithObjects:@"application/json",@"text/json", @"text/plain", @"text/html", nil];
+    //    manager.responseSerializer.acceptableContentTypes = [NSSet setWithObject:@"text/html"];
+    //    manager.responseSerializer.acceptableContentTypes = [NSSet setWithObject:@"text/plain; charset=utf-8"];
+    //    manager.responseSerializer.acceptableContentTypes = [manager.responseSerializer.acceptableContentTypes setByAddingObject:@"text/plain"];
+    
+    
+    //SEND YOUR REQUEST
+    [manager POST:postUrl parameters:parameters success:^(AFHTTPRequestOperation *operation, id responseObject) {
+        NSLog(@"JSON: %@", responseObject);
+        
+        NSString *str = [responseObject objectForKey:@"KEY 1"];
+        NSArray *arr = [responseObject objectForKey:@"KEY 2"];
+        NSDictionary *dic = [responseObject objectForKey:@"KEY 3"];
+        
+        //...
+    } failure:^(AFHTTPRequestOperation *operation, NSError *error) {
+        NSLog(@"Error: %@", error);
+    }];
+ 
+     */
+
     NSLog(@"5555");
     PayReq *request = [[PayReq alloc] init];
     request.partnerId = @"10000100";
@@ -35,6 +77,21 @@
     request.timeStamp= @"1397527777".intValue ;
     request.sign= @"582282D72DD2B03AD892830965F428CB16E7A256";
     [WXApi sendReq:request];
+// 
+//    NSLog(@"weiXinPay:(id)sender ");
+//    [self performSegueWithIdentifier:@"AA" sender:self];
 }
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender{
+    NSLog(@"prepareForSegue!");
+    UIViewController *vcTemp = (UIViewController*)segue.destinationViewController;
+    if ([vcTemp respondsToSelector:@selector(setStrX:)]) {
+        [vcTemp setValue:@"123abcxxx" forKey:@"strX"];
+    }
+}
+
+//-(void)performSegueWithIdentifier:(NSString *)identifier sender:(id)sender{
+//    NSLog(@"performSegueWithIdentifier!");
+//    [super performSegueWithIdentifier:identifier sender:self];
+//}
 
 @end
